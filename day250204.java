cdfe1047409165f4b79bb6e7454d348fdd2115f8
kimsj18 <kimsj0118@gmail.com>
2025-02-05 11:39:14 +0900

    0204

--- day250204.java
+++ day250204.java
@@ -7,6 +7,7 @@ public class day250204 {
 
         //예외가 발생하였을때 일단 정상실행할수 있도록 처리하는 코드를 예외코드라 한다.
         // try-catch , try-catch-finally
+
         try{
             Class.forName("java.lang.String");
             System.out.println("클래스 존재");
@@ -16,10 +17,34 @@ public class day250204 {
             //e.printStackTrace();                    //예외 정보를 얻는 방법3
         }
 
+        String[] stringarray = {"100","l00"};
+        for(int i=0 ; i<stringarray.length+1 ; i++) {
+            try {
+                int value = Integer.parseInt(stringarray[i]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("배열 인덱스 초과" + e.getMessage());
+            }catch (NullPointerException | NumberFormatException e1){
+                System.out.println("숫자 변환 못함" + e1.getMessage());
+            }
+        }
 
-
-
+        //예외 발생시키기 (throw)
+        // try-catch로 예외처리하는 것이 아닌 메소드를 호출한 곳으로 떠넘기기(throw)
+        try {
+            findClass();
+        } catch (Throwable e) {
+            System.out.println("예외처리" + e.getMessage());    //호출한 곳에서 예외 처리
+        }
 
     }
 
+        public static void findClass() throws Exception{
+            Class.forName("java.lang.String1");
+    }
+
+
+
 }
+
+
+
